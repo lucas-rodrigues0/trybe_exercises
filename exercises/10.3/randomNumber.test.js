056c2c1bb7b5361d657b2f163ab27f3e5c49f42b
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-01-06 15:36:11 -0300

    conclui o requisito 2

--- exercises/10.3/randomNumber.test.js
+++ exercises/10.3/randomNumber.test.js
@@ -1,16 +1,27 @@
 const func = require('./randomNumber');
+jest.mock("./randomNumber");
 
 describe('Test the randomNumber function', () => {
   it('should test the function with a mock function', () => {
-    func.randomNumber = jest.fn().mockReturnValue(10);
+    func.randomNumber.mockReturnValue(10);
 
     expect(func.randomNumber).toHaveBeenCalledTimes(0);
 
     func.randomNumber();
     expect(func.randomNumber).toHaveBeenCalled();
-    
+
     expect(func.randomNumber).toHaveBeenCalledTimes(1);
     expect(func.randomNumber()).toBe(10);
     expect(func.randomNumber).toHaveBeenCalledTimes(2);
+  });
+
+  it('should test the function with a mock implementation', () => {
+    func.randomNumber.mockImplementation((a, b) => a / b);
+    func.randomNumber(10, 2);
+
+    expect(func.randomNumber).toHaveBeenCalled();
+    expect(func.randomNumber).toHaveBeenCalledTimes(3);
+    expect(func.randomNumber).toHaveBeenCalledWith(10, 2);
+    expect(func.randomNumber(10, 2)).toBe(5);
   })
 });
