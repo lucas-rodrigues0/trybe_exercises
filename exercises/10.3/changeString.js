05c7704ab85e0cdc1d97921a45b3d85029957523
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-01-06 18:34:35 -0300

    conclui o requisito 4 e 5

---
+++ exercises/10.3/changeString.js
@@ -0,0 +1,11 @@
+const upperCase = str => str.toUpperCase();
+
+const firstLetter = str => str.charAt(0);
+
+const joinStrings = (str1, str2) => str1.concat(str2);
+
+module.exports = {
+  upperCase,
+  firstLetter,
+  joinStrings
+}
