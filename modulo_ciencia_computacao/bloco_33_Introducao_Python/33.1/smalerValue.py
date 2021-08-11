d04239a19e371846cb97115ec0fb32fa52f35493
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-08-11 16:17:45 -0300

    Implementa exercicio Bonus 1

---
+++ modulo_ciencia_computacao/bloco_33_Introducao_Python/33.1/smalerValue.py
@@ -0,0 +1,12 @@
+def smaler_value(values):
+    result = values[0]
+    for value in values:
+        if value < result:
+            result = value
+
+    return result
+
+
+if __name__ == "__main__":
+    test = [5, 9, 19, 70, 8, 100, 35, 27]
+    print(smaler_value(test))
