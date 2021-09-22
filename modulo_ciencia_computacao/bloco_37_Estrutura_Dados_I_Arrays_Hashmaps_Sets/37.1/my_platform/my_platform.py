4d15d017ade3f06cda2636ad5330cfbaf1bb944a
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-09-22 14:49:50 -0300

    Exercicios do dia 37.1

---
+++ modulo_ciencia_computacao/bloco_37_Estrutura_Dados_I_Arrays_Hashmaps_Sets/37.1/my_platform/my_platform.py
@@ -0,0 +1,5 @@
+import platform
+
+print("The platform used is:", platform.platform())
+print("The version used is:", platform.version())
+print("The architecture used is:", platform.architecture())
